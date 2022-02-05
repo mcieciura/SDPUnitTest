a921269a30e45e53ff75a8f51bea6b0c62ef13a4
mcieciura <61789293+mcieciura@users.noreply.github.com>
2022-02-05 03:39:31 -0800

    homework

---
+++ CheckPrime/CheckPrime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CheckPrime
+{
+        public static class CheckPrimes
+        {
+            public static bool IsPrime(int number)
+            {
+                if (number < 0)
+                {
+                    throw new ArgumentException("Number must be positive!");
+                }
+                if (number == 0 || number == 1)
+                {
+                    return false;
+                }
+                for (int i = 2; i <= number / 2; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+}
---
+++ TestPrime/TestPrime.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AutoFixture;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using CheckPrime;
+
+namespace TestPrime
+{
+    public class TestPrime
+    {
+        [Test]
+        public void ExceptionNegativeValue()
+        {
+            Assert.Throws<ArgumentException>(() => CheckPrimes.IsPrime(-5));
+        }
+
+        [TestCase(0, false)]
+        [TestCase(1, false)]
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(4, false)]
+        [TestCase(5, true)]
+        [TestCase(6, false)]
+        [TestCase(7, true)]
+        [TestCase(8, false)]
+        [TestCase(9, false)]
+        public void CheckValue(int value, bool expectedValue)
+        {
+
+            var actualValue = CheckPrimes.IsPrime(value);
+
+            actualValue.Should().Be(expectedValue);
+        }
+    }
+}
